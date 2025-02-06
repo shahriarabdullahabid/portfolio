32dd2ec91f96b41887eb50a49dac8773f649743c
Shahriar Abid <abidshahriar283@gmail.com>
2025-02-06 17:49:10 +0600

    Fix Vite config to use ES modules

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { LaravelVitePlugin } from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel({
+        new LaravelVitePlugin({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
